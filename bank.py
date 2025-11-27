bed98a619d15c40f60b81015fce84ebeb343fdf5
ritikakathare <ritikakathare15@gmail.com>
2025-11-27 15:25:38 +0530

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,7 @@
+import sys
+script_name = sys.argv[0]
+initial_bal =float(sys.argv[1])
+deposit_amount =float(sys.argv[2])
+updated_bal = initial_bal + deposit_amount
+print("Initial balance:",initial_bal)
+print("deposit balance:",deposit_amount)
