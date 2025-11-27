c9502059c3bf4c17451d46604d64ab2e0a32fc7c
ritikakathare <ritikakathare15@gmail.com>
2025-11-27 15:32:37 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,7 +1,13 @@
 import sys
+
 script_name = sys.argv[0]
-initial_bal =float(sys.argv[1])
-deposit_amount =float(sys.argv[2])
+
+
+initial_bal = float(sys.argv[1])
+deposit_amount = float(sys.argv[2])
+
 updated_bal = initial_bal + deposit_amount
-print("Initial balance:",initial_bal)
-print("deposit balance:",deposit_amount)
+
+print("Initial Balance:", initial_bal)
+print("Deposit Amount:", deposit_amount)
+print("Updated Balance:", updated_bal)
